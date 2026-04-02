4f70d0bb8d2c88fb7c20d94227502a7ccb7172e2
dhruvc308 <dhruv.chaudhary.career@gmail.com>
2026-04-03 03:46:42 +0530

    Create day01_variables.py
    Day 1 practice task: Write a script that stores your name, age, company in variables and prints them.

---
+++ day01_variables.py
@@ -0,0 +1,15 @@
+# Storing personal details in variables
+name = "Dhruv"
+age = 23
+company = "AmexGBT"
+# Printing the details
+print("Name:", name)
+print("Age:", age)
+print("Company:", company)
+
+
+# Using formatted output
+name = "Dhruv"
+age = 23
+company = "AmexGBT"
+print(f"My name is {name}, I am {age} years old, and I work at {company}.")
